39f6889fddd879a23e5154d90f035b1e4b007fb3
Thomas Pimentel <thomas@webscaping.ca>
2020-10-12 18:43:06 -0400

    Completed todo backend

--- modules/app/__init__.py
+++ modules/app/__init__.py
@@ -5,7 +5,8 @@ import datetime
 from bson.objectid import ObjectId
 from flask import Flask
 from flask_pymongo import PyMongo
-
+from flask_jwt_extended import JWTManager
+from flask_bcrypt import Bcrypt
 
 class JSONEncoder(json.JSONEncoder):
     """ extend json-encoder class """
@@ -23,8 +24,11 @@ app = Flask(__name__)
 
 # add mongo url to flask config so flask_pymongo can establish the connection.
 app.config['MONGO_URI'] = os.environ.get('DB')
+app.config['JWT_SECRET_KEY'] = os.environ.get('SECRET')
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = datetime.timedelta(days=1)
 mongo = PyMongo(app)
-
+jwt = JWTManager(app)
+app.json_encoder = JSONEncoder
 # use the modified encoder class to handle ObjectId & datetime object while jsonifying the response.
 app.json_encoder = JSONEncoder
 
